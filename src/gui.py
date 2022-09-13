fed2373134f97f96aade799304f65b0c988b42be
eduardo-reyes <eduardoreyes@ciencias.unam.mx>
2022-09-12 20:41:48 -0500

    Arreglo gui

--- src/gui.py
+++ src/gui.py
@@ -1,42 +1,45 @@
 from argparse import MetavarTypeHelpFormatter
 from asyncore import read
 from tkinter import  Tk, Button, Entry, Label,PhotoImage,Frame, OptionMenu, StringVar
-from PIL import Image 
-import requests       
+from tkinter.ttk import Combobox
+from PIL import Image       
 import time
 import os
-from classes import Ticket, WeatherInfo
+from classes import Ticket
 import readcsv
 
 
 cache = {}
+cities = []
 myTickets = readcsv.getTickets()
 
 def get_weather(ticket: Ticket):
-    origin_city = ticket.get_origin_city()
-    destination_city = ticket.get_destination_city()
-    my_weather_origin = ' '
-    my_weather_destination = ' '
+	origin_city = ticket.get_origin_city()
+	destination_city = ticket.get_destination_city()
+	my_weather_origin = ' '
+	my_weather_destination = ' '
 
-    if origin_city in cache:
-        my_weather_origin = cache[origin_city]
-    else:
-        my_weather_origin = ticket.get_weather_origin()
-        cache[origin_city] = my_weather_origin
+	if origin_city in cache:
+		my_weather_origin = cache[origin_city]
+	else:
+		my_weather_origin = ticket.get_weather_origin()
+		cache[origin_city] = my_weather_origin
+		cities.append(origin_city)
     
-    if destination_city in cache:
-        my_weather_destination = cache[destination_city]
-    else:
-        my_weather_destination = ticket.get_weather_destination()
-        cache[destination_city] = my_weather_destination
-    
-    return my_weather_origin, my_weather_destination
+	if destination_city in cache:
+		my_weather_destination = cache[destination_city]
+	else:
+		my_weather_destination = ticket.get_weather_destination()
+		cache[destination_city] = my_weather_destination
+		cities.append(destination_city)
 
+	return my_weather_origin, my_weather_destination
 
 for ticket in myTickets:
     if ticket.get_origin_city() == 'origin':
         continue
     weather1, weather2 = get_weather(ticket)
+
 
 #weather1, weather2 = get_weather(myTickets[5])
 
@@ -52,18 +55,14 @@ class Window(Frame):
 		self.master.rowconfigure(2, weight=1)
 		self.frame = Frame(self.master, bg='white', highlightbackground='grey1',highlightthickness=2)
 		self.frame.grid(columnspan=3, row = 0, sticky='nsew', padx=5, pady=5)
-		self.frame2 = Frame(self.master, bg='yellow green', highlightbackground='grey1',highlightthickness=2)
-		self.frame2.grid(column=0, row = 1, sticky='nsew', padx=5, pady=5)
-		self.frame3 = Frame(self.master, bg='orange2', highlightbackground='grey1',highlightthickness=2) 
-		self.frame3.grid(column=1, row = 1, sticky='nsew', padx=5, pady=5)
-		self.frame4 = Frame(self.master, bg='light cyan', highlightbackground='grey1',highlightthickness=2)
-		self.frame4.grid(column=2, row = 1, sticky='nsew', padx=5, pady=5)
-		self.frame5 = Frame(self.master, bg='peach puff', highlightbackground='grey1',highlightthickness=2) 
-		self.frame5.grid(column=0, row = 2, sticky='nsew', padx=5, pady=5)
-		self.frame6 = Frame(self.master, bg='royal blue', highlightbackground='grey1',highlightthickness=2)
-		self.frame6.grid(column=1, row = 2, sticky='nsew', padx=5, pady=5)
-		self.frame7 = Frame(self.master, bg='azure2', highlightbackground='grey1',highlightthickness=2) 
-		self.frame7.grid(column=2, row =2 , sticky='nsew', padx=5, pady=5)
+		self.frame2 = Frame(self.master, bg='DarkOliveGreen1', highlightbackground='grey1',highlightthickness=2)
+		self.frame2.grid(columnspan=3, row = 1, sticky='nsew', padx=5, pady=5)
+		self.frame3 = Frame(self.master, bg='peach puff', highlightbackground='grey1',highlightthickness=2) 
+		self.frame3.grid(column=0, row = 2, sticky='nsew', padx=5, pady=5)
+		self.frame4 = Frame(self.master, bg='royal blue', highlightbackground='grey1',highlightthickness=2)
+		self.frame4.grid(column=1, row = 2, sticky='nsew', padx=5, pady=5)
+		self.frame5 = Frame(self.master, bg='azure2', highlightbackground='grey1',highlightthickness=2) 
+		self.frame5.grid(column=2, row =2 , sticky='nsew', padx=5, pady=5)
 
 		self.widgets()
 
@@ -73,11 +72,8 @@ class Window(Frame):
 		city = city.upper()
 
 		if city in cache:
-
 			weather_info = cache[city].get_attributes_as_list()
-			self.temp['text'] = str(float(weather_info[0])) + " °C"
-			self.temp_min['text'] = str(float(weather_info[1]))  +" °C"
-			self.temp_max['text'] = str(float(weather_info[2])) +" °C"
+			self.temp['text'] = "Temperatura " + str(float(weather_info[0])) + " °C\n\n" + "T máxima " + str(float(weather_info[1]))  +" °C\n\n" + "T mínima " +str(float(weather_info[2])) +" °C"
 			self.feels_like['text'] = str(float(weather_info[3])) + " °C"
 			self.humidity['text'] = str(int(weather_info[4])) + ' %'	  
 			self.description['text'] = weather_info[5]
@@ -85,8 +81,6 @@ class Window(Frame):
 		else:
 			self.warning['text'] =  'Ciudad no encontrada'
 			self.temp['text'] = ''
-			self.temp_min['text'] = ''
-			self.temp_max['text'] = ''
 			self.feels_like['text'] = ''
 			self.description['text'] = ''
 			self.humidity['text'] = ''
@@ -94,83 +88,49 @@ class Window(Frame):
 			time.sleep(2)	    	
 			self.warning['text'] = ''
 			self.place['text'] = ''	
-
-		""""
-		API = 'https://api.openweathermap.org/data/2.5/weather?q=' +city+ '&appid=a6c823e88b813b8ba68e3508021fc9ec&units=metric&lang=es'  
-		try:
-			json_datos = requests.get(API).json()
-			self.temp['text'] = str(int(json_datos['main']['temp'])) + " °C"
-			self.temp_min['text'] = str(int(json_datos['main']['temp_min']))  +" °C"
-			self.temp_max['text'] = str(int(json_datos['main']['temp_max'])) +" °C"
-			self.feels_like['text'] = str(int(json_datos['main']['feels_like'])) + " °C"
-			self.humidity['text'] = str(json_datos['main']['humidity']) + ' %'	  
-			self.description['text'] = json_datos['weather'][0]['description']  
-			self.place['text'] =  json_datos['name'] + ' - '+ json_datos['sys']['country'] 
-		except:
-			self.warning['text'] =  'Ciudad no encontrada'
-			self.temp['text'] = ''
-			self.temp_min['text'] = ''
-			self.temp_max['text'] = ''
-			self.feels_like['text'] = ''
-			self.description['text'] = ''
-			self.humidity['text'] = ''
-			self.master.update()
-			time.sleep(2)	    	
-			self.warning['text'] = ''
-			self.place['text'] = ''	
-		"""
 
 	def widgets(self):
 		absolute_folder_path = os.path.dirname(os.path.realpath(__file__))
 		absolute_path_search = os.path.join(absolute_folder_path, 'images/search.png')
 		self.start = PhotoImage(file =absolute_path_search)
-		absolute_path_temp = os.path.join(absolute_folder_path, 'images/temp.png')
+		absolute_path_temp = os.path.join(absolute_folder_path, 'images/info.png')
 		self.image_temp = PhotoImage(file =absolute_path_temp)
-		absolute_path_tempmin = os.path.join(absolute_folder_path, 'images/temp_min.png')
-		self.image_temp_min = PhotoImage(file =absolute_path_tempmin)
-		absolute_path_tempmax = os.path.join(absolute_folder_path, 'images/temp_max.png')
-		self.image_temp_max = PhotoImage(file =absolute_path_tempmax)
 		absolute_path_feelslike = os.path.join(absolute_folder_path, 'images/feelslike.png')
 		self.image_feelslike = PhotoImage(file =absolute_path_feelslike)
 		absolute_path_humidity = os.path.join(absolute_folder_path, 'images/humidity.png')
 		self.image_humidity = PhotoImage(file =absolute_path_humidity)
 		absolute_path_description = os.path.join(absolute_folder_path, 'images/climate.png')
 		self.image_description = PhotoImage(file =absolute_path_description)
-		self.bt_start = Button(self.frame, image= self.start, bg='OliveDrab1',highlightthickness=0, activebackground='white', bd=0, command = self.get_weather)
-		self.bt_start.grid(column=0, row=0, padx=2, pady=2)
+		Label(self.frame,text='Buscar ciudad',fg= 'gray55', bg='white',font=('Verdana',12)).grid(column=0,row=0, padx=5)
 		self.enter_city = Entry(self.frame, font=('Verdana', 14),highlightbackground = "grey1", highlightcolor= "green2", highlightthickness=2)
 		self.enter_city.grid(column=1,row=0)
-		Label(self.frame,text='Buscar ciudad',fg= 'gray55', bg='white',font=('Verdana',12)).grid(column=2,row=0, padx=5)
+		self.bt_start = Button(self.frame, image= self.start, bg='OliveDrab1',highlightthickness=0, activebackground='white', bd=0, command = self.get_weather)
+		self.bt_start.grid(column=2, row=0, padx=2, pady=2)
+		Label(self.frame,text='Ciudades disponibles',fg= 'gray55', bg='white',font=('Verdana',12)).grid(column=3,row=0, padx=5)
+		self.city_list = Combobox(self.frame, state = "readonly", values=cities,font=('Helvetica',12,'bold'))
+		self.city_list.grid(column=4, row=0)
 		self.warning = Label(self.frame,fg= 'red3', bg='white',font=('Verdana',12))
-		self.warning.grid(column=3,row=0, padx=5)
+		self.warning.grid(column=5,row=0, padx=5)
 		self.place = Label(self.frame,fg= 'forest green', bg='white',font=('Helvetica',12,'bold'))
-		self.place.grid(column=4,row=0, padx=5)
+		self.place.grid(column=6,row=0, padx=5)
 
-		Label(self.frame2,text='Temperatura', bg='yellow green', font=('Helvetica',14,'bold')).pack(expand=1)
-		Label(self.frame3,text='T máxima', bg='orange2', font=('Helvetica',14,'bold')).pack(expand=1)
-		Label(self.frame4,text='T mínima' , bg='light cyan', font=('Helvetica',14,'bold')).pack(expand=1)
-		Label(self.frame5,text='Sensación térmica' , bg='peach puff', font=('Helvetica',14,'bold')).pack(expand=1)
-		Label(self.frame6,text='Humedad' , bg='royal blue', font=('Helvetica',14,'bold')).pack(expand=1)
-		Label(self.frame7,text='Descripción' , bg='azure2', font=('Helvetica',14,'bold')).pack(expand=1)
+		Label(self.frame2,text='Informe del clima', bg='DarkOliveGreen1', font=('Helvetica',20,'bold')).pack(expand=1)
+		Label(self.frame3,text='Sensación térmica', bg='peach puff', font=('Helvetica',14,'bold')).pack(expand=1)
+		Label(self.frame4,text='Humedad' , bg='royal blue', font=('Helvetica',14,'bold')).pack(expand=1)
+		Label(self.frame5,text='Descripción' , bg='azure2', font=('Helvetica',14,'bold')).pack(expand=1)
 
-		Label(self.frame2, image= self.image_temp, bg='yellow green').pack(expand=1, side='left')
-		Label(self.frame3, image= self.image_temp_max, bg='orange2').pack(expand=1, side='left')
-		Label(self.frame4, image= self.image_temp_min, bg='light cyan').pack(expand=1, side='left')
-		Label(self.frame5, image= self.image_feelslike, bg='peach puff').pack(expand=1, side='left')
-		Label(self.frame6, image= self.image_humidity, bg='royal blue').pack(expand=1, side='left')
-		Label(self.frame7, image= self.image_description, bg='azure2').pack(expand=1, side='left')
+		Label(self.frame2, image= self.image_temp, bg='DarkOliveGreen1').pack(expand=1, side='left')
+		Label(self.frame3, image= self.image_feelslike, bg='peach puff').pack(expand=1, side='left')
+		Label(self.frame4, image= self.image_humidity, bg='royal blue').pack(expand=1, side='left')
+		Label(self.frame5, image= self.image_description, bg='azure2').pack(expand=1, side='left')
 
-		self.temp = Label(self.frame2, bg='yellow green', font = "Helvetica 16 bold")
+		self.temp = Label(self.frame2, bg='DarkOliveGreen1', font = "Helvetica 16 bold")
 		self.temp.pack(expand=1, side='right')
-		self.temp_max = Label(self.frame3,bg='orange2', font = "Helvetica 16 bold")
-		self.temp_max.pack(expand=1, side='right')
-		self.temp_min = Label(self.frame4, bg='light cyan', font = "Helvetica 16 bold")
-		self.temp_min.pack(expand=1, side='right')
-		self.feels_like = Label(self.frame5, bg='peach puff', font = "Helvetica 16 bold")
+		self.feels_like = Label(self.frame3,bg='peach puff', font = "Helvetica 16 bold")
 		self.feels_like.pack(expand=1, side='right')
-		self.humidity = Label(self.frame6, bg='royal blue', font = "Helvetica 16 bold")
+		self.humidity = Label(self.frame4, bg='royal blue', font = "Helvetica 16 bold")
 		self.humidity.pack(expand=1, side='right')
-		self.description = Label(self.frame7, bg='azure2', font = "Helvetica 16 bold")
+		self.description = Label(self.frame5, bg='azure2', font = "Helvetica 16 bold")
 		self.description.pack(expand=1, side='right')
 
 if __name__ == "__main__":
