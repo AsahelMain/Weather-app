a49ff804ee3bf4c09a0b9951a894d267c6530a16
AsahelMain <asahelmain@ciencias.unam.mx>
2022-09-23 11:48:36 -0500

    Eliminación del módulo classes.py. Creación de la clase ReadCSV

--- src/readcsv.py
+++ src/readcsv.py
@@ -7,31 +7,33 @@ import csv
 import os.path
 from airport import Airport
 
-airports_list = []
+class ReadCSV:
+    
+    airports_list = []
 
-def getAirports():
-    airports = {}
+    def getAirports(self):
+        airports = {}
 
-    current_directory = os.path.dirname(__file__)
-    parent_directory = os.path.split(current_directory)[0]
+        current_directory = os.path.dirname(__file__)
+        parent_directory = os.path.split(current_directory)[0]
 
-    with open(parent_directory + '\dataset1.csv', 'r') as file:
-        reader = csv.reader(file)
-        next(reader) 
-        for row in reader:
-            iata_code1 = row[0]
-            iata_code2 = row[1]
+        with open(parent_directory + '\dataset1.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader) 
+            for row in reader:
+                iata_code1 = row[0]
+                iata_code2 = row[1]
 
-            if iata_code1 not in airports:
-                airport_object = Airport(row[0], row[2], row[3])
-                airports[iata_code1] = airport_object
-                airports_list.append(iata_code1)
-            if iata_code2 not in airports:
-                airport_object = Airport(row[1], row[4], row[5])
-                airports[iata_code2] = airport_object
-                airports_list.append(iata_code2)
+                if iata_code1 not in airports:
+                    airport_object = Airport(row[0], row[2], row[3])
+                    airports[iata_code1] = airport_object
+                    self.airports_list.append(iata_code1)
+                if iata_code2 not in airports:
+                    airport_object = Airport(row[1], row[4], row[5])
+                    airports[iata_code2] = airport_object
+                    self.airports_list.append(iata_code2)
 
-    return airports
+        return airports
 
-def get_airports_list():
-    return airports_list
+    def get_airports_list(self):
+        return self.airports_list
