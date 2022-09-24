374c7587aa0b231252e187b112bd2f93678cad96
eduardo-reyes <eduardoreyes@ciencias.unam.mx>
2022-09-23 19:27:36 -0500

    Arreglo ruta csv

--- src/readcsv.py
+++ src/readcsv.py
@@ -13,7 +13,7 @@ class ReadCSV:
         current_directory = os.path.dirname(__file__)
         parent_directory = os.path.split(current_directory)[0]
 
-        with open(parent_directory + '\dataset1.csv', 'r') as file:
+        with open(parent_directory + '/dataset1.csv', 'r') as file:
             reader = csv.reader(file)
             next(reader) 
             for row in reader:
